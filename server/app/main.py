361f1bcc64115e889cfeb7831005f0d86fb0708b
Hector Miranda <hector.miranda8@upr.edu>
2021-10-19 14:01:25 -0400

    Flask db setup

--- server/app/main.py
+++ server/app/main.py
@@ -1,16 +1,20 @@
 # pip install Flask
 from flask import Flask, jsonify, request
 # pip install sqlalchemy, flask_sqlalchemy
-# from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ggdbuahlhppdpj:868aecec8547c09e6bf38bccf743ca92bf41ea71b2b490af20f0472a6aee8fff@ec2-54-209-187-69.compute-1.amazonaws.com:5432/d4dtc4du1mree0'
-# app.debug = True
-# db = SQLalchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ggdbuahlhppdpj:868aecec8547c09e6bf38bccf743ca92bf41ea71b2b490af20f0472a6aee8fff@ec2-54-209-187-69.compute-1.amazonaws.com:5432/d4dtc4du1mree0'
+app.debug = True
+db = SQLAlchemy(app)
 
-@app.route('/test', method=['GET'])
+@app.route('/test', methods=['GET'])
 def test():
     return {
         'test': 'test1'
     }
+
+@app.route('/', methods=['GET'])
+def home_view():
+    return "<h1>Welcome to PosVibesDB </h1>"
---
+++ server/app/models/testTable.py
@@ -0,0 +1,12 @@
+from ..main import db
+
+class testTable(db.Model):
+    __tablename__ = 'testTable'
+    testTitle = db.Column(db.String(100), primary_key=True)
+    testText = db.Column(db.String(), nullable=False)
+    testLikes = db.Column(db.String(), nullable=False, default=0)
+
+    def __init__(self, tTitle, tText, tLikes):
+        self.testTitle = tTitle
+        self.testText = tText
+        self.testLikes = tLikes
