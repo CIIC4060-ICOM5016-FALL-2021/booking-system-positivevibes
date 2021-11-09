0d35194a6f8a6c026d563096c3f843e40dc38867
HectorMiranda <hector.miranda8@upr.edu>
2021-11-09 13:36:10 -0800

    Removed wsgi

--- server/wsgi.py
+++
@@ -1,8 +0,0 @@
-import app
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
-
-if __name__ == '__main__':
-    app.run()
