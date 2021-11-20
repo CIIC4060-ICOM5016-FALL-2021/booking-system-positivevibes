a87140892f778375fb8919d163469fba8658a7c1
Hector Miranda <hector.miranda8@upr.edu>
2021-11-20 19:14:26 -0400

    Frontend Setup

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {Route, Navigate, BrowserRouter, Routes} from 'react-router-dom';
+import HomePage from "./HomePage";
+import BookMeeting from "./BookMeeting";
+import 'semantic-ui-css/semantic.min.css'
+import UserView from "./UserView";
+import Dashboard from "./Dashboard";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/Home" element={<HomePage/>} />
+            <Route exact path="/UserView" element={<UserView/>} />
+            <Route exact path="/Dashboard" element={<Dashboard/>} />
+            <Route path="*" element={<Navigate to ="/Home" />}/>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
