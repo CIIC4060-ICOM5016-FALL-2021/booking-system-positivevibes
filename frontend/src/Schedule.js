4ee475b1524cd6c5a8384f1d94543f1ddf58c238
Jomar-Santos <jomar.santos@upr.edu>
2021-12-06 14:19:50 -0400

    Schedule

--- frontend/src/Schedule.js
+++ frontend/src/Schedule.js
@@ -34,31 +34,20 @@ const user = JSON.parse(localStorage.getItem('User'));
 function Schedule(){
 
     const [dates, setDates] = useState([]); // current selected 'event'
-    const [roomDates, setRoomDates] = useState([]); // holds parsed r_unavails
+    const [userDates, setUserDates] = useState([]); // holds parsed r_unavails
     const localizer = momentLocalizer(moment);
-    const [rooms, setRooms] = useState([]); // get from rooms
-    const [selectedRoom, setSelectedRoom] = useState({'text': 'Select Room', 'value': -1});
-    const [sucessText, setSucessText] = useState("");
     const [selected, setSelected] = useState({}); // selected event
     const [allSchedules, setAllSchedules] = useState([]); // get from schedules
     const [showModify, setShowModify] = useState(false); // boolean to show event popup modification
-    const [deleteBtn, setDeleteBtn] = useState('hidden'); // delete room visibility
-    const [deletePop, setDeletePop] = useState(false); // delete confirmation popup
     const [appointedRoom, setAppointedRoom] = useState({}); // who appointed a room (user)
-    const [allInvitees, setAllInvitees] = useState([]); // holds all invitees of selected event
+    //const [allInvitees, setAllInvitees] = useState([]); // holds all invitees of selected event
     const [inviteeEmails, setInviteeEmails] = useState(""); // holds all invitee emails of selected event
-    const [authLvlText, setAuthLvlText] = useState("<Please Select a Room>"); // to display the auth lvl of room
 
-    const [addRoomOpen, setAddRoomOpen] = useState(false); // boolean to show add room form
-    const [modRoomOpen, setModRoomOpen] = useState(false); // boolean to show modify room form
-
-    
     useEffect(() => {
-        setRoomDates([]);
-        setDeleteBtn('visible');
-        changeAuthText();
+        setUserDates([]);
+
         setTimeout(() => {}, 250);
-        let room_dates = []
+        let user_dates = []
         let room_selected = {
             //room_id : selectedRoom.value,
             user_id : user.user_id,
@@ -93,40 +82,29 @@ function Schedule(){
                 // new Date(year, month, day, hours, minutes, seconds, milliseconds)
                 let start_t = new Date(date_split[0], date_split[1]-1, date_split[2], start_split[0], start_split[1], start_split[2], 0);
                 let end_t = new Date(date_split[0], date_split[1]-1, date_split[2], end_split[0], end_split[1], end_split[2], 0);
-                room_dates.push({
+                user_dates.push({
                     'title': tmp[i].slot_name,
                     'allDay': false,
                     'start': start_t,
                     'end': end_t
                 });
             }
-            setRoomDates(room_dates);
+            setUserDates(user_dates);
         })
         axios({method: 'GET', url: CONFIG.URL + '/schedule'})
         .then((res) => setAllSchedules(res.data));
     }, []);
 
-    const changeAuthText = () => {
-        // setAuthLvlText(authDictionary[selectedRoom.value.toString()]);
-        for(let i = 0; i < rooms.length; i++) {
-            if (rooms[i].value == selectedRoom.value) {
-                setAuthLvlText(authDictionary[rooms[i].authorization_level.toString()]);
-                break;
-            }
-        }
-    }
-
 
     const handleSelectedEvent = (event) => {
         setSelected(event);
         setShowModify(true);
-        setAllInvitees([]);
+        //setAllInvitees([]);
         setInviteeEmails("");
 
         // fetch who appointed the room
         let e_info = parseFromDate(event.start, event.end);
         let params = {
-            "room_id" : selectedRoom.value,
             "user_id" : user.user_id,
             "date" : e_info[0],
             "time" : e_info[1]
@@ -149,15 +127,9 @@ function Schedule(){
                 params: params,
                 url: CONFIG.URL + '/rooms/appointed'
             })
-            .then((res) => {
-                setAppointedRoom(res.data)
-                console.log("Data")
-                console.log(res.data)
-            })
+            .then((res) => setAppointedRoom(res.data))
             axios({method: 'GET', url: CONFIG.URL + '/invitee/schedule/' + sched_id.toString()})
             .then((res) => {
-                console.log(appointedRoom)
-                setAllInvitees(res.data);
                 let emailString = ""
                 for (let i = 0; i < res.data.length; i++) {
                     if (i != res.data.length - 1) 
@@ -180,16 +152,6 @@ function Schedule(){
     }
         
 
-    const deleteRoomHelper = () => {
-        axios({method: 'DELETE', url: room_url + '/' + selectedRoom.value.toString()})
-        .then((res) => {
-            setDeletePop(false);
-            setSucessText('Room deleted! Refreshing page...');
-            setTimeout(() => window.location.reload(), 2500);
-        })
-        .catch((err) => deleteRoomHelper()) // please do not do this elsewhere c:
-    }
-
     return (
     
     
@@ -200,7 +162,7 @@ function Schedule(){
             onSelectEvent={handleSelectedEvent}
             localizer={localizer}
             startAccessor="start"
-            events={roomDates.concat(dates)}
+            events={userDates.concat(dates)}
             endAccessor="end"
             views={["month", "day"]}
             defaultDate={Date.now()}
@@ -235,61 +197,8 @@ function Schedule(){
             </Modal.Actions>
         </Modal>
 
-
-        
-        <Modal
-            centered={true}
-            open={addRoomOpen}
-            onClose={() => setAddRoomOpen(false)}
-            onOpen={() => setAddRoomOpen(true)}         
-            >
-            <Modal.Content>
-                <AddRoom/>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button onClick={() => setAddRoomOpen(false)}>CLOSE</Button>
-            </Modal.Actions>
-        </Modal>
-
-        <Modal
-            centered={true}
-            open={modRoomOpen}
-            onClose={() => setModRoomOpen(false)}
-            onOpen={() => setModRoomOpen(true)}         
-            >
-            <Modal.Content>
-                <ModifyRoom/>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button onClick={() => setModRoomOpen(false)}>CLOSE</Button>
-            </Modal.Actions>
-        </Modal>
-        
     </Container>
     )
-        
-
-
-    // const [dates, setDates] = useState([{
-    //     'title': 'Selection',
-    //     'allDay': false,
-    //     'start': new Date(moment.now()),
-    //     'end': new Date(moment.now())
-    // }]);
-    // const [open, setOpen] = useState(false);
-    // const localizer = momentLocalizer(moment)
-
-    // return <Container style={{ height: 800 }}><Calendar
-    //     localizer={localizer}
-    //     startAccessor="start"
-    //     events={dates}
-    //     endAccessor="end"
-    //     views={["month", "day"]}
-    //     defaultDate={Date.now()}
-    // >
-
-    // </Calendar>
-    // </Container>
 
 
 }
